798da2466a628bc53dd16fa2c1abf4a4c0501d90
Sanyam Sharma <106426986+sanyamsharmaa@users.noreply.github.com>
2024-04-29 09:47:18 +0530

    image correction

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -1,7 +1,7 @@
 const ProjectList = [ 
     {
         name:"Idea- Pack : Make your thoughts into reality",
-        img: "/Ideapack.png",
+        img: "./Ideapack.png",
         link: "https://ideapack.000webhostapp.com/",
         gitlnk:"https://github.com/sanyamsharmaa/Idea-Pack-Pack-your-ideas",
         desp: "Idea Pack is platform where you can list your short notes,to do tasks, project ideas and business ideas.You can easily create, edit and delete them. It is a fully integrated with database which stores your activities."
@@ -9,7 +9,7 @@ const ProjectList = [
     },
     {
         name:"Nwscape",
-        img: "/Nwscape.png",
+        img: "./Nwscape.png",
         link: "",
         gitlnk:"https://github.com/sanyamsharmaa/Nwscape",
         desp: "Nwsape is powerful news app delivers the latest headlines for easy access.It have categorized news,so delve into Indian news, or get your fill of health, tech, entertainment, business, sports, and science updates. Plus, the powerful search bar lets you explore any topic that sparks your curiosity"
@@ -17,7 +17,7 @@ const ProjectList = [
     },
     {
         name:"Color-Generator",
-        img: "/Colorgenerator.png",
+        img: "./Colorgenerator.png",
         link: "https://sanyamsharmaa.github.io/Color-Generator/",
         gitlnk:"https://github.com/sanyamsharmaa/Color-Generator",
         desp: "You can simply get 15+ shades of a color on one click, you need to sipmply paste the rgb/hex code of your color and boom! you get its different shades"
@@ -25,7 +25,7 @@ const ProjectList = [
     },
     {
         name:"TextUtils",
-        img: "/TextUtils.png",
+        img: "./TextUtils.png",
         link: "https://sanyamsharmaa.github.io/TextUtils",
         gitlnk:"https://github.com/sanyamsharmaa/TextUtils",
         desp: "Textutils gives you a way to analyze your text quickly and efficiently, word count, character count or reading time"
@@ -33,7 +33,7 @@ const ProjectList = [
     },
     {
         name:"Artify:listen-any-artist",
-        img: "/artifyPic.png",
+        img: "./artifyPic.png",
         link: "https://artify-listen-any-artist.onrender.com/",
         gitlnk:"https://github.com/sanyamsharmaa/Artify-Listen-any-artist",
         desp: "This is a music exploration platform that leverages the Spotify Web API. Search for any artist and you'll be redirected to Spotify to their profiles where you can all of their songs effortlessly."
@@ -41,7 +41,7 @@ const ProjectList = [
     },
     {
         name:"Supub-cafe menu",
-        img: "/Cafemenu.png",
+        img: "./Cafemenu.png",
         link: "https://sanyamsharmaa.github.io/CafeMenu/",
         gitlnk:"https://github.com/sanyamsharmaa/CafeMenu",
         desp: "You can see the menu of the famous cafe through CafeMenu. It show various categories like coldrink, hotdrink, pastries, fast food etc"
